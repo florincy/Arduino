1f5f626e06cf8a553b736ea961f383e8e258d194
florincy <36941768+florincy@users.noreply.github.com>
2018-08-13 18:45:56 -0300

    De novo!

---
+++ Leitura/Leitura.ino
@@ -0,0 +1,21 @@
+#include <Thermistor.h>
+Thermistor termistor(A5);
+int pinLDR = 4;
+int TValue = 0;
+int LDRValue = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.print("LDR ");
+  Serial.println ("Temp. (°C)");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int TValue = termistor.getTemp(); 
+  LDRValue = analogRead(pinLDR);
+  Serial.print (LDRValue);
+  Serial.print (",");
+  Serial.println (TValue);
+  delay (1500);
+}
